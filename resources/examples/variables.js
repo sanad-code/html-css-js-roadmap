b69e2eb0c681eeae5f3cc728a19bf0698a73bcb1
Moataz Sanad <sanad.code.io@gmail.com>
2025-03-14 14:24:46 +0200

    js

--- resources/examples/variables.js
+++ resources/examples/variables.js
@@ -13,14 +13,59 @@
 // person.name = "Abc";
 // console.log(person);
 
-console.log("123" == 123);
+// console.log("123" == 123);
 
-console.log("123" === 123);
+// console.log("123" === 123);
 
-console.log(Boolean("123"));
-console.log(Boolean(0));
-console.log(Boolean(""));
-console.log(Boolean(null));
-console.log(Boolean(undefined));
-console.log(Boolean([]));
-console.log(Boolean({}));
+// console.log(Boolean("123"));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+
+let age = 5;
+console.log(`age is ${age}`);
+age = 6;
+console.log(`age is ${age}`);
+let newAge = age;
+newAge = 7;
+console.log(`age is ${age}`);
+console.log(`newAge is ${newAge}`);
+
+const person = { name: "msanad", age: 25 };
+console.log(`person is ${person.age}`);
+person.age = 26;
+console.log(`person is ${person.age}`);
+
+const newPerson = person;
+newPerson.age = 27;
+console.log(`person is ${person.age}`);
+console.log(`newPerson is ${newPerson.age}`);
+
+let sayHi = function () {
+  let age = 8;
+  console.log(`age is ${age}`);
+};
+sayHi();
+
+console.log(`age is ${age}`);
+
+// let age = 5;
+// age = 6;
+// let newAge = age;
+// newAge = 7;
+
+// const person = { name: "msanad", age: 25 };
+// person.age = 26;
+
+// const newPerson = person;
+// newPerson.age = 27;
+
+// let sayHi = function () {
+//   let age = 8;
+//   console.log(`age is ${age}`);
+// };
+
+// sayHi();
